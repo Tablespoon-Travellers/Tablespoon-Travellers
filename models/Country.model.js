5ad7e4066dc73288a5189eede65df2b9391e6d2d
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-09-10 13:08:30 +0200

    Added country model

---
+++ models/Country.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const countrySchema = new Schema({
+	name: { type: String },
+	description: { type: String },
+    playlistId: { type: String },
+    recipeUrl: { type: String },
+	imageUrl: {
+		type: String,
+		default:
+			'https://images.unsplash.com/photo-1513694203232-719a280e022f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80'
+	},
+	
+	
+	reviews: [{type: Schema.Types.ObjectId, ref: 'Review', default: []}] // ID String for the review, not for the user or comment
+	/**
+	 * 	user: ID String, // If you populate level 2, you will get the complete info about the User
+	    comment: String
+	 */
+});
+
+const Country = model('Country', countrySchema);
+
+module.exports = Country;
