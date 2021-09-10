f07ab42f27842a98f6c420e2e4a9af2686bc2cdf
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-09-10 12:23:46 +0200

    fix navbar and add project title

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var app = express();
 require('./config/db')
 require('./config/global')(app)
 
+// default value for title local
+const projectName = 'project2';
+const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+
+app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+
 const isLoggedIn = require('./middleware/isLoggedIn');
 
 const authRoutes = require('./routes/auth.routes');
