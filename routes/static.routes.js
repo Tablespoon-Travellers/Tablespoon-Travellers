6bc5b00e8fbc17a827ffccec9a9d1d0529677d94
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-09-13 20:31:34 +0200

    WIP spinner and better error message

--- routes/static.routes.js
+++ routes/static.routes.js
@@ -1,24 +1,35 @@
 const express = require('express');
+
+const Country = require("../models/Country.model");
+
 const router = express.Router();
 
 router.get('/404', (req, res) => {
-	res.render('static/404');
+    res.render('static/404');
 });
 
 router.get('/500', (req, res) => {
-	res.render('static/500');
+    res.render('static/500');
 });
 
 router.get('/spin', (req, res) => {
-	res.render('Wheel spinner');
+    Country
+    .find()
+    .then((allCountries) => {
+        res.render('Wheel spinner', {countries: JSON.stringify(allCountries)});
+    })
+    .catch((error) => { 
+        console.log("Something bad happened:", error)
+        throw error
+    });
 });
 
 router.get('/landing-page', (req, res) => {
-	res.render('static/landing-page');
+    res.render('static/landing-page');
 });
 
 router.get('/map', (req, res) => {
-	res.render('static/map');
+    res.render('static/map');
 });
 
 
