a893a8b0057b2fa7d8d6aa25e24157f4e76724aa
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-09-13 20:01:09 +0200

    Add routing for 404 and 500 pages

--- app.js
+++ app.js
@@ -48,13 +48,17 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
+  if (err.status === 404) {
+    res.render("static/404");
+    return
+  }
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("static/500");
 });
 
 module.exports = app;
